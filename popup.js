b70402340bf35928d5e3f270970aad5284d4c66f
Ignacio Streuly <isjtreuly@gmail.com>
2016-06-16 08:59:54 -0400

    Initial commit

---
+++ popup.js
@@ -0,0 +1,32 @@
+SC.initialize({
+client_id: "672dd37d370de6fc46011a19cd62660d",
+});
+
+var stream = new Audio();
+var paused = false;
+
+var counter = 0;
+var urls = ["https://soundcloud.com/samsin111/thug-juice","https://soundcloud.com/weirdinside/breathing-as-we-know-ep-out-211-vinyl-pre-order-211", "https://soundcloud.com/quietluke/blue-day-3"];
+
+function resolveAndPlayURL() {
+  SC.get("https://api.soundcloud.com/resolve/?url=" + urls[counter], {limit: 1}, function(result){
+
+      var xhr = new XMLHttpRequest();
+
+      client_id = '?client_id=d4ab52d80ed2e7790c3a243495b30093';
+	  xhr.open('GET', result.uri + client_id);
+	  xhr.onload = function(){
+  		var track = JSON.parse(xhr.responseText);
+  		stream.src = track.stream_url + client_id;
+  		stream.play();
+	  }; 
+	  xhr.send()
+  });
+}
+
+
+
+  
+
+
+ 
