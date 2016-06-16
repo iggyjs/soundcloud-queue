934525733c02dc294fdc1e588ec9bae1ad22612b
Ignacio Streuly <ijstreuly@gmail.com>
2016-06-16 09:16:05 -0400

    attempting to have SDK script running persistently

--- main.js
+++ main.js
@@ -1,36 +1,3 @@
 $(document).ready(function(){
-	console.log("updated")
-  	$("#play").on('click', function(){
-	  	resolveAndPlayURL();  	
-	});
-
-	$("#pause").on('click', function(){
-	  stream.pause();
-	});
-
-	 // $("#pause").on('click', function(){
-
-	 //  	if (!paused) {
-	 //  		stream.pause();
-	 //  		paused = true;
-	 //  	} else {
-	 //  		stream.play();
-	 //  		paused = false;
-	 //  	}
-	 //  });
-
-	  $("#forward").on('click', function(){
-	  	if (counter != (urls.length-1) ){
-	  		counter ++;
-	  		resolveAndPlayURL();
-	  	}
-	  });
-
-	  $("#back").on('click', function(){
-	  	if (counter != 0){
-	  		counter --;
-	  		resolveAndPlayURL();
-	  	}
-	  });
 
 });
--- popup.js
+++ popup.js
@@ -24,9 +24,35 @@ function resolveAndPlayURL() {
   });
 }
 
+$("#play").on('click', function(){
+    resolveAndPlayURL();    
+});
 
+$("#pause").on('click', function(){
+  stream.pause();
+});
 
-  
+ // $("#pause").on('click', function(){
 
+ //   if (!paused) {
+ //     stream.pause();
+ //     paused = true;
+ //   } else {
+ //     stream.play();
+ //     paused = false;
+ //   }
+ //  });
 
- 
+  $("#forward").on('click', function(){
+    if (counter != (urls.length-1) ){
+      counter ++;
+      resolveAndPlayURL();
+    }
+  });
+
+  $("#back").on('click', function(){
+    if (counter != 0){
+      counter --;
+      resolveAndPlayURL();
+    }
+  });
