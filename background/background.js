84a3d40060cb3f3bbd064790f978f259c732e9db
Ignacio Streuly <ijstreuly@gmail.com>
2016-07-02 12:44:19 -0400

    Got chrome messages working between background and content scripts

--- background/background.js
+++ background/background.js
@@ -1,11 +1,10 @@
-chrome.runtime.onConnect.addListener(function(port) {
-  console.assert(port.name == "knockknock");
-  port.onMessage.addListener(function(msg) {
-    if (msg.joke == "Knock knock")
-      port.postMessage({question: "Who's there?"});
-    else if (msg.answer == "Madame")
-      port.postMessage({question: "Madame who?"});
-    else if (msg.answer == "Madame... Bovary")
-      port.postMessage({question: "I don't get it."});
+function sendMessage(tabId, message, callback){
+  chrome.tabs.sendMessage(tabId, {ping: true}, function(response){
+    chrome.tabs.sendMessage(tabId, message, callback);
   });
+}
+
+chrome.tabs.query({url: "http://example.com/"}, function(tabs) {
+  console.log("message sent");
+  sendMessage(tabs[0].id, {greeting: "hello"});
 });
--- content/content_script.js
+++ content/content_script.js
@@ -1,6 +1,11 @@
-console.log('called');
-$.get(chrome.extension.getURL('templates/notification.html'), function(data) {
-    var div = $("div");
-    div.append(data);
+console.log(chrome);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if(request.ping) { sendResponse({ping: true}); return; }
+  console.log(request);
 });
-
+// console.log('called');
+// $.get(chrome.extension.getURL('templates/notification.html'), function(data) {
+//     var div = $("div");
+//     div.append(data);
+// });
+// //
