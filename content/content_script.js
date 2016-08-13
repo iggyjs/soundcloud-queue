2fbac485154d44fedc3aa2e72ce66ba09fbea069
Ignacio Streuly <ijstreuly@gmail.com>
2016-08-13 15:49:04 -0400

    Add: Button as vue component

--- content/content_script.js
+++ content/content_script.js
@@ -1,12 +1,42 @@
 // // TODO: Add buttons to soundcloud.com
-var buttons = document.getElementsByClassName("sc-button-group");
+var buttons = document.getElementsByClassName("soundActions");
+
+
+function QueueFunctionExtension(){
+	alert("wokring");
+}
 
 window.onload = function (){
-	var p = "<button>hacked</button>"
-	console.log(p);
+	var ButtonComponent = Vue.extend({
+	  template: '<button>{{buttonText}}</button >'
+	})
+
+
+	counter = 0;
+	
 	for(var i=0; i<buttons.length; i++){	
+		var button = new ButtonComponent({
+		  data: {
+		    buttonText: 'test'
+		  },
+		  methods: {
+		  	handleClickTest: function(){
+		  		alert('working');
+		  	}
+		  }
+
+		})
+
+		var p = "<div v-on:click='handleClickTest' class='mount-point" + counter + " button-component-queue'></div>"
+
 		buttons[i].insertAdjacentHTML('afterend', p);
+		button.$mount('.mount-point' + counter);
+		counter++;
+
 	}	
+
+
+	
 }
 
 window.onscroll = function(ev) {
@@ -19,3 +49,5 @@ function checkForMoreButtons(){
 	var b = document.getElementsByClassName("sc-button-group");
 	console.log(buttons);
 }
+
+
