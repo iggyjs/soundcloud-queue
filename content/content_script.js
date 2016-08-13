721c34d1798ed60a2778805bf616087d66b5f130
Ignacio Streuly <ijstreuly@gmail.com>
2016-08-13 16:30:14 -0400

    Add: Got button comp reliably loading

--- content/content_script.js
+++ content/content_script.js
@@ -1,37 +1,42 @@
 // // TODO: Add buttons to soundcloud.com
-var buttons = document.getElementsByClassName("soundActions");
-
+var buttons = document.getElementsByClassName("sound__soundActions");
+var globalScrollCounter = 0;
 
 function QueueFunctionExtension(){
 	alert("wokring");
 }
 
 window.onload = function (){
+	// FIXME lazyload starts before the bottom of the page
+	window.onscroll = function(ev) {
+	    if ((window.innerHeight + window.scrollY + 500) >= document.body.offsetHeight) {
+	        setTimeout(checkForMoreButtons, 3500);
+	    }
+	};
+
 	var ButtonComponent = Vue.extend({
 	  template: '<button>{{buttonText}}</button >'
 	})
-
-
-	counter = 0;
 	
-	for(var i=0; i<buttons.length; i++){	
+	for(var i=0; i<buttons.length; i++){
+
 		var button = new ButtonComponent({
 		  data: {
-		    buttonText: 'test'
+		    buttonText: 'Add'
 		  },
 		  methods: {
 		  	handleClickTest: function(){
-		  		alert('working');
+		  		console.log(this);
 		  	}
 		  }
 
 		})
 
-		var p = "<div v-on:click='handleClickTest' class='mount-point" + counter + " button-component-queue'></div>"
+		var p = "<div v-on:click='handleClickTest' class='mount-point" + globalScrollCounter + " button-component-queue'></div>"
 
 		buttons[i].insertAdjacentHTML('afterend', p);
-		button.$mount('.mount-point' + counter);
-		counter++;
+		button.$mount('.mount-point' + globalScrollCounter);
+		globalScrollCounter++;
 
 	}	
 
@@ -39,15 +44,35 @@ window.onload = function (){
 	
 }
 
-window.onscroll = function(ev) {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        setTimeout(checkForMoreButtons, 2000);
-    }
-};
-
 function checkForMoreButtons(){
-	var b = document.getElementsByClassName("sc-button-group");
-	console.log(buttons);
+	var ButtonComponent = Vue.extend({
+	  template: '<button>{{buttonText}}</button >'
+	})
+
+	var b = document.getElementsByClassName("sound__soundActions"); 
+
+	console.log(globalScrollCounter);	
+
+	for(var i=globalScrollCounter; i<buttons.length; i++){
+
+		var button = new ButtonComponent({
+		  data: {
+		    buttonText: 'test'
+		  },
+		  methods: {
+		  	handleClickTest: function(){
+		  		console.log(this);
+		  	}
+		  }
+
+		})
+
+		var p = "<div v-on:click='handleClickTest' class='mount-point" + globalScrollCounter + " button-component-queue'></div>"
+
+		buttons[i].insertAdjacentHTML('afterend', p);
+		button.$mount('.mount-point' + globalScrollCounter);
+		globalScrollCounter++;
+	}	
 }
 
 
